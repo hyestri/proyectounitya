540580dd71edcda181378d2bf7109193d99253a0
hyestri <hyestri@gmail.com>
2015-11-20 11:44:38 -0500

    escena pacman - personaje - pickups - supercookie

---
+++ Assets/script/character.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movment = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movment);
+
+	}
+}
---
+++ Assets/script/supercookie.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class supercookie : cookie {
+	
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right, 4f);
+	}
+}
